9f2ad5516a2942f2824768207c819e07b27be5ad
yvtapir <yvtapir@gmail.com>
2026-05-27 13:51:09 +0300

    deploy

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,27 +4,21 @@ pragma solidity ^0.8.18;
 import {Script, console2} from "forge-std/Script.sol";
 
 import {StrategyFactory} from "../src/StrategyFactory.sol";
-import {IStrategyInterface} from "../src/interfaces/IStrategyInterface.sol";
 
 contract Deploy is Script {
     address public constant DEFAULT_ROUTER =
         0xE592427A0AEce92De3Edee1F18E0157C05861564;
 
-    // Set these before broadcasting.
-    address public management;
-    address public performanceFeeRecipient;
-    address public keeper;
-    address public emergencyAdmin;
+    function run() external returns (StrategyFactory factory) {
+        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        address management = vm.envAddress("MANAGEMENT");
+        address performanceFeeRecipient = vm.envAddress(
+            "PERFORMANCE_FEE_RECIPIENT"
+        );
+        address keeper = vm.envAddress("KEEPER");
+        address emergencyAdmin = vm.envAddress("EMERGENCY_ADMIN");
 
-    // Leave these zeroed if you only want the factory.
-    address public asset;
-    address public morphoVaultV2;
-    string public strategyName = "";
-    address public router = DEFAULT_ROUTER;
-
-    function run() external returns (StrategyFactory factory, address strategy) {
-
-        vm.startBroadcast();
+        vm.startBroadcast(privateKey);
 
         factory = new StrategyFactory(
             management,
@@ -33,14 +27,7 @@ contract Deploy is Script {
             emergencyAdmin
         );
         console2.log("factory", address(factory));
-
-        strategy = factory.newStrategy(
-            asset,
-            strategyName,
-            morphoVaultV2,
-            router
-        );
-        console2.log("strategy", strategy);
+        console2.log("default router", DEFAULT_ROUTER);
 
         vm.stopBroadcast();
     }
